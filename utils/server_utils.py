46400fc516dc7bd8dec402938580fe177c3a35dd
neilsambhu <33429095+neilsambhu@users.noreply.github.com>
2022-08-15 16:25:40 -0400

    Apptainer->docker

--- utils/server_utils.py
+++ utils/server_utils.py
@@ -45,8 +45,8 @@ class CarlaServerManager():
                     port += 5
 
     def start(self):
-        kill_carla()
-        # kill_carla_docker()
+        # kill_carla()
+        kill_carla_docker()
         for cfg in self.env_configs:
             cmd = f'CUDA_VISIBLE_DEVICES={cfg["gpu"]} bash {self._carla_sh_str} ' \
                 f'-fps=10 -quality-level=Epic -carla-rpc-port={cfg["port"]}'
@@ -78,7 +78,7 @@ class CarlaServerManager():
             cmd = f'echo q | sudo -S docker run --privileged --gpus "device={cfg["gpu"]}" --net=host -v /tmp/.X11-unix:/tmp/.X11-unix:rw carlasim/carla:0.9.13 /bin/bash ./CarlaUE4.sh -carla-rpc-port={cfg["port"]} -RenderOffScreen'
             # 8/9/2022: Neil added: end
             # 8/11/2022: Neil added: start 
-            cmd = f'apptainer build --sandbox carla-0.9.13/ docker://carlasim/carla:0.9.13 && bash carla-0.9.13/home/carla/CarlaUE4.sh'
+            # cmd = f'apptainer build --sandbox carla-0.9.13/ docker://carlasim/carla:0.9.13 && bash carla-0.9.13/home/carla/CarlaUE4.sh'
             # 8/11/2022: Neil added: end 
             log.info(cmd)
             # log_file = self._root_save_dir / f'server_{cfg["port"]}.log'
@@ -87,8 +87,8 @@ class CarlaServerManager():
                 frameinfo = getframeinfo(currentframe());print(f"Neil {frameinfo.filename}:{frameinfo.lineno}")
             server_process = subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid) # 8/9/2022 4:51:00 PM: comment out 
             # 8/9/2022: Dan added: start
-            # server_process = subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid, stdin=subprocess.PIPE, encoding="utf8")
-            # sudo_out = server_process.communicate('q\n', timeout=1)
+            server_process = subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid, stdin=subprocess.PIPE, encoding="utf8")
+            sudo_out = server_process.communicate('q\n', timeout=1)
             # 8/9/2022: Dan added: end
             if bVerbose:
                 frameinfo = getframeinfo(currentframe());print(f"Neil {frameinfo.filename}:{frameinfo.lineno}")

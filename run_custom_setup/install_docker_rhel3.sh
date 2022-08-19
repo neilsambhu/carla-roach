8b54a26f3d6f6fe9cf2b01cee4e65da58f5139d3
Neil Sambhu <nsambhu@localhost.localdomain>
2022-08-19 16:40:01 -0400

    install docker through rpm

---
+++ run_custom_setup/install_docker_rhel3.sh
@@ -0,0 +1,18 @@
+curl -O https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-20.10.9-3.el7.x86_64.rpm &&
+curl -O https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-cli-20.10.9-3.el7.x86_64.rpm &&
+curl -O https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.6.7-3.1.el7.x86_64.rpm &&
+curl -O https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-compose-plugin-2.6.0-3.el7.x86_64.rpm &&
+# additional packages
+curl -O https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-scan-plugin-0.17.0-3.el7.x86_64.rpm &&
+curl -O https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-rootless-extras-20.10.17-3.el7.x86_64.rpm &&
+
+# Install Docker Engine
+# sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo yum localinstall *.rpm &&
+
+rm *.rpm &&
+
+## Start Docker.
+sudo systemctl start docker &&
+## Verify that Docker Engine is installed correctly by running the hello-world image.
+sudo docker run hello-world &&

e816da099199257f2fe1968f8d53216cf9bee9c4
neilsambhu <33429095+neilsambhu@users.noreply.github.com>
2022-08-19 17:41:02 -0400

    Update install_carla_python_whl.sh

--- run_custom_setup/install_carla_python_whl.sh
+++ run_custom_setup/install_carla_python_whl.sh
@@ -2,7 +2,7 @@ cd run_custom_setup
 mkdir install_carla_python_whl
 cd install_carla_python_whl
 
-curl -O https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/CARLA_0.9.13.tar.gz
+# curl -O https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/CARLA_0.9.13.tar.gz
 ls *.tar.gz
 
 tar –xvzf CARLA_0.9.13.tar.gz

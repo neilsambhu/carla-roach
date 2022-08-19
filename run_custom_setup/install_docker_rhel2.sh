9f426cf4c3d5d7aeaf37e8885e08b28edc6b9ed8
Neil Sambhu <nsambhu@localhost.localdomain>
2022-08-19 16:22:18 -0400

    install docker through rpm

--- run_custom_setup/install_docker_rhel2.sh
+++ run_custom_setup/install_docker_rhel2.sh
@@ -2,10 +2,13 @@ curl -O https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker
 curl -O https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-cli-20.10.9-3.el7.x86_64.rpm
 curl -O https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.6.7-3.1.el7.x86_64.rpm
 curl -O https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-compose-plugin-2.6.0-3.el7.x86_64.rpm
+
 # Install Docker Engine
 # sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo yum localinstall *.rpm
+
 rm *.rpm
+
 ## Start Docker.
 sudo systemctl start docker
 ## Verify that Docker Engine is installed correctly by running the hello-world image.

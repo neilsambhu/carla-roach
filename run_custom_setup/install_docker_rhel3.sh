d730ec80b1927fa25c06d0cf1552ec2c7fdb4af5
neilsambhu <33429095+neilsambhu@users.noreply.github.com>
2022-08-19 16:52:11 -0400

    Update install_docker_rhel3.sh

--- run_custom_setup/install_docker_rhel3.sh
+++ run_custom_setup/install_docker_rhel3.sh
@@ -4,11 +4,11 @@
 # subscription-manager list --available    Find valid RHEL pool ID
 # subscription-manager attach --pool=pool_id
 
-subscription-manager repos --enable=rhel-7-server-rpms &&
-subscription-manager repos --enable=rhel-7-server-extras-rpms &&
-subscription-manager repos --enable=rhel-7-server-optional-rpms &&
+sudo subscription-manager repos --enable=rhel-7-server-rpms &&
+sudo subscription-manager repos --enable=rhel-7-server-extras-rpms &&
+sudo subscription-manager repos --enable=rhel-7-server-optional-rpms &&
 
-yum install docker device-mapper-libs device-mapper-event-libs &&
+sudo yum install docker device-mapper-libs device-mapper-event-libs &&
 
-systemctl start docker.service &&
-systemctl enable docker.service &&
+sudo systemctl start docker.service &&
+sudo systemctl enable docker.service &&

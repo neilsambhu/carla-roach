d1a549faee9f81a09f40a0f08bc37ec6bf3ca8ef
neilsambhu <33429095+neilsambhu@users.noreply.github.com>
2022-08-20 15:53:07 -0400

    Update install_nvidia-docker2_rhel.sh

--- run_custom_setup/install_nvidia-docker2_rhel.sh
+++ run_custom_setup/install_nvidia-docker2_rhel.sh
@@ -1,2 +1,7 @@
 # https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html#docker
-ls
+sudo yum-config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum repolist -v
+sudo yum install -y https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.4.3-3.1.el7.x86_64.rpm
+sudo yum install docker-ce -y
+sudo systemctl --now enable docker
+sudo docker run --rm hello-world

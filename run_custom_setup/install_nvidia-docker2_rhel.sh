fe602ff1c853590790a45f6bb94a5ae9d348c742
Neil Sambhu <neilsambhu@gmail.com>
2022-08-20 15:48:33 -0400

    nvidia-docker2

---
+++ run_custom_setup/install_nvidia-docker2_rhel.sh
@@ -0,0 +1,15 @@
+# https://access.redhat.com/discussions/3199282
+# https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux_atomic_host/7/html-single/getting_started_with_containers/index#getting_docker_in_rhel_7
+
+# subscription-manager list --available    Find valid RHEL pool ID
+# subscription-manager attach --pool=pool_id
+
+sudo subscription-manager repos --enable=rhel-7-server-rpms &&
+sudo subscription-manager repos --enable=rhel-7-server-extras-rpms &&
+sudo subscription-manager repos --enable=rhel-7-server-optional-rpms &&
+
+sudo yum install docker device-mapper-libs device-mapper-event-libs &&
+
+sudo systemctl start docker.service &&
+sudo systemctl enable docker.service &&
+:

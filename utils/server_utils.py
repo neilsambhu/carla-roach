0a435bee1e507e72d64a3a699390c41fca56c788
Neil Sambhu <neilsambhu@gmail.com>
2022-09-13 15:28:52 -0400

    lazy

--- utils/server_utils.py
+++ utils/server_utils.py
@@ -88,10 +88,10 @@ class CarlaServerManager():
             if bVerbose:
                 frameinfo = getframeinfo(currentframe());print(f"Neil {frameinfo.filename}:{frameinfo.lineno}")
             server_process = subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid) # 8/9/2022 4:51:00 PM: comment out 
-            # 8/9/2022: Dan added: start
+            # 8/9/2022: Dan added (for docker): start
             server_process = subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid, stdin=subprocess.PIPE, encoding="utf8")
             sudo_out = server_process.communicate('q\n', timeout=1)
-            # 8/9/2022: Dan added: end
+            # 8/9/2022: Dan added (for docker): end
             # 8/26/2022: Neil added: start
             time.sleep(10)
             # 8/26/2022: Neil added: end

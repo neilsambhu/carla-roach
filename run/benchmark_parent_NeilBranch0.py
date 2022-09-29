0962dd6364c5c5d0746d21d8de794ff6480f6f65
Neil Sambhu <neilsambhu@gmail.com>
2022-09-28 23:19:22 -0400

    automate results:3 seeds

--- run/benchmark_parent_NeilBranch0.py
+++ run/benchmark_parent_NeilBranch0.py
@@ -27,11 +27,6 @@ def DeleteScoreFiles():
 		os.remove("score_composed.txt")
 	if os.path.exists("score_route.txt"):
 		os.remove("score_route.txt")
-def CheckScoreFiles():
-	if os.path.exists("score_composed.txt") and os.path.exists("score_route.txt"):
-		return True
-	else:
-		return False
 def DeleteCheckpointFiles():
 	if os.path.exists("outputs/checkpoint.txt"):
 		os.remove("outputs/checkpoint.txt")
@@ -51,15 +46,8 @@ if __name__ == '__main__':
 		DeleteScoreFiles()
 		PPO_exp_NCd_tt = BenchmarkConfiguration(agent="ppo",wb_group="PPO+exp",wb_notes="Benchmark PPO+exp on NoCrash-dense-tt.",test_suites="nocrash_dense_tt",seed=seed,
 			wb_sub_group="nocrash_dense_tt-2021",agent_ppo_wb_run_path="iccv21-roach/trained-models/1929isj0")
-		# benchmarkProcess = subprocess.run(["python","-u","benchmark_NeilBranch0.py","resume=true","log_video=true","wb_project=iccv21-roach-benchmark",f'agent={PPO_exp_NCd_tt.agent}',f'actors.hero.agent={PPO_exp_NCd_tt.agent}',f'agent.ppo.wb_run_path={PPO_exp_NCd_tt.agent_ppo_wb_run_path}',f'wb_group=\"{PPO_exp_NCd_tt.wb_group}\"',f'wb_notes="{PPO_exp_NCd_tt.wb_notes}"',f'test_suites={PPO_exp_NCd_tt.test_suites}',f'seed={PPO_exp_NCd_tt.seed}',f'+wb_sub_group={PPO_exp_NCd_tt.wb_sub_group}',"no_rendering=true"],timeout=secondsIn365Days)
-		# benchmarkProcess = subprocess.run(["python","-u","benchmark_NeilBranch0.py","resume=true","log_video=true","wb_project=iccv21-roach-benchmark",f'agent={PPO_exp_NCd_tt.agent}',
-		# 	f'actors.hero.agent={PPO_exp_NCd_tt.agent}',f'agent.ppo.wb_run_path={PPO_exp_NCd_tt.agent_ppo_wb_run_path}',f'wb_group=\"{PPO_exp_NCd_tt.wb_group}\"',
-		# 	f'wb_notes="{PPO_exp_NCd_tt.wb_notes}"',f'test_suites={PPO_exp_NCd_tt.test_suites}',f'seed={PPO_exp_NCd_tt.seed}',f'+wb_sub_group={PPO_exp_NCd_tt.wb_sub_group}',
-		# 	"no_rendering=true"],shell=True,timeout=secondsIn365Days)
 		benchmarkProcess = subprocess.Popen([f'python -u benchmark_NeilBranch0.py resume=true log_video=true wb_project=iccv21-roach-benchmark agent={PPO_exp_NCd_tt.agent} actors.hero.agent={PPO_exp_NCd_tt.agent} agent.ppo.wb_run_path={PPO_exp_NCd_tt.agent_ppo_wb_run_path} wb_group=\"{PPO_exp_NCd_tt.wb_group}\" wb_notes="{PPO_exp_NCd_tt.wb_notes}" test_suites={PPO_exp_NCd_tt.test_suites} seed={PPO_exp_NCd_tt.seed} +wb_sub_group={PPO_exp_NCd_tt.wb_sub_group} no_rendering=true'],shell=True)
 		benchmarkProcess.wait()
-		# while not CheckScoreFiles():
-		# 	time.sleep(15)
 		with open("score_composed.txt","r") as f:
 			score_composed.append(float(f.read()))
 		with open("score_route.txt","r") as f:

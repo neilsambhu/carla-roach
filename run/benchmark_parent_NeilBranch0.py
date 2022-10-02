82642068f37f4aadb67eec03c2b7ae4dd4af198f
Neil Sambhu <neilsambhu@gmail.com>
2022-10-01 21:08:14 -0400

    automate inference nearly complete

--- run/benchmark_parent_NeilBranch0.py
+++ run/benchmark_parent_NeilBranch0.py
@@ -19,19 +19,26 @@ class BenchmarkConfiguration:
     def stringConfiguration(self):
         return f'{self.wb_group} {self.test_suites}'
     def average_score_composed(self):
-        return round(sum(self.score_composed)/len(self.score_composed))
+        return round(sum(self.score_composed)/len(self.score_composed),4)
     def average_score_route(self):
         return round(sum(self.score_route)/len(self.score_route),4)
     def standardDeviation_score_composed(self):
         return round(statistics.pstdev(self.score_composed),4)
     def standardDeviation_score_route(self):
         return round(statistics.pstdev(self.score_route),4)
+    def StartBenchmarkProcess(self,seed):
+        benchmarkProcess = None
+        if self.agent=="ppo":
+            benchmarkProcess = subprocess.Popen([f'python -u benchmark_NeilBranch0.py resume=true log_video=true wb_project=iccv21-roach-benchmark agent={self.agent} actors.hero.agent={self.agent} agent.ppo.wb_run_path={self.agent_ppo_wb_run_path} wb_group=\"{self.wb_group}\" wb_notes="{self.wb_notes}" test_suites={self.test_suites} seed={seed} +wb_sub_group={self.wb_sub_group} no_rendering=true'],shell=True)
+        elif self.agent=="roaming":
+            benchmarkProcess = subprocess.Popen([f'python -u benchmark_NeilBranch0.py resume=true log_video=true wb_project=iccv21-roach-benchmark agent={self.agent} actors.hero.agent={self.agent} +agent/roaming/obs_configs=birdview wb_group=\"{self.wb_group}\" wb_notes="{self.wb_notes}" test_suites={self.test_suites} seed={seed} +wb_sub_group={self.wb_sub_group} no_rendering=true'],shell=True)
+        return benchmarkProcess
     def Benchmark(self):
         for seed in [2021,2022,2023]:
             DeleteCheckpointFiles()
             self.wb_sub_group = f'{self.test_suites}-{seed}'
             print(f'benchmark configuration: {self.wb_group} {self.wb_sub_group}')
-            benchmarkProcess = subprocess.Popen([f'python -u benchmark_NeilBranch0.py resume=true log_video=true wb_project=iccv21-roach-benchmark agent={self.agent} actors.hero.agent={self.agent} agent.ppo.wb_run_path={self.agent_ppo_wb_run_path} wb_group=\"{self.wb_group}\" wb_notes="{self.wb_notes}" test_suites={self.test_suites} seed={seed} +wb_sub_group={self.wb_sub_group} no_rendering=true'],shell=True)
+            benchmarkProcess = self.StartBenchmarkProcess(seed)
             benchmarkProcess.wait()
             with open("score_composed.txt","r") as f:
                 self.score_composed.append(float(f.read()))
@@ -62,19 +69,19 @@ def DeleteResultsFile():
         os.remove("results.txt")
 def GenerateBenchmarkConfigurations():
     benchmarkConfigurations = []
-    for environment in ["tt","tn","nt"]
-        # PPO+exp: NCd-tt
-        PPO_exp_NCd_tt = BenchmarkConfiguration(agent="ppo",wb_group="PPO+exp",wb_notes="Benchmark PPO+exp on NoCrash-dense-tt.",test_suites="nocrash_dense_tt",agent_ppo_wb_run_path="iccv21-roach/trained-models/1929isj0")
-        benchmarkConfigurations.append(PPO_exp_NCd_tt)
-        # PPO+exp: NCd-tn
-        PPO_exp_NCd_tn = BenchmarkConfiguration(agent="ppo",wb_group="PPO+exp",wb_notes="Benchmark PPO+exp on NoCrash-dense-tn.",test_suites="nocrash_dense_tn",agent_ppo_wb_run_path="iccv21-roach/trained-models/1929isj0")
-        benchmarkConfigurations.append(PPO_exp_NCd_tn)
-        # PPO+exp: NCd-nt
-        PPO_exp_NCd_nt = BenchmarkConfiguration(agent="ppo",wb_group="PPO+exp",wb_notes="Benchmark PPO+exp on NoCrash-dense-nt.",test_suites="nocrash_dense_nt",agent_ppo_wb_run_path="iccv21-roach/trained-models/1929isj0")
-        benchmarkConfigurations.append(PPO_exp_NCd_nt)
-        # PPO+exp: NCd-nn
-        PPO_exp_NCd_tn = BenchmarkConfiguration(agent="ppo",wb_group="PPO+exp",wb_notes="Benchmark PPO+exp on NoCrash-dense-nn.",test_suites="nocrash_dense_nn",agent_ppo_wb_run_path="iccv21-roach/trained-models/1929isj0")
-        benchmarkConfigurations.append(PPO_exp_NCd_nn)
+    for environment in ["tt","tn","nt","nn"]:
+        # PPO+exp: NCd
+        PPO_exp_NCd = BenchmarkConfiguration(agent="ppo",wb_group="PPO+exp",wb_notes=f'Benchmark PPO+exp on NoCrash-dense-{environment}.',test_suites=f'nocrash_dense_{environment}',agent_ppo_wb_run_path="iccv21-roach/trained-models/10pscpih")
+        benchmarkConfigurations.append(PPO_exp_NCd)
+        # PPO+beta: NCd
+        PPO_beta_NCd = BenchmarkConfiguration(agent="ppo",wb_group="PPO+beta",wb_notes=f'Benchmark PPO+beta on NoCrash-dense-{environment}.',test_suites=f'nocrash_dense_{environment}',agent_ppo_wb_run_path="iccv21-roach/trained-models/1ch63m76")
+        benchmarkConfigurations.append(PPO_beta_NCd)
+        # Roach: NCd
+        Roach_NCd = BenchmarkConfiguration(agent="ppo",wb_group="Roach",wb_notes=f'Benchmark Roach on NoCrash-dense-{environment}.',test_suites=f'nocrash_dense_{environment}',agent_ppo_wb_run_path="iccv21-roach/trained-models/1929isj0")
+        benchmarkConfigurations.append(Roach_NCd)
+        # Autopilot: NCd
+        Autopilot_NCd = BenchmarkConfiguration(agent="roaming",wb_group="Autopilot",wb_notes=f'Benchmark Autopilot on NoCrash-dense-{environment}.',test_suites=f'nocrash_dense_{environment}')
+        benchmarkConfigurations.append(Roach_NCd)
     return benchmarkConfigurations
 if __name__ == '__main__':
     DeleteResultsFile()

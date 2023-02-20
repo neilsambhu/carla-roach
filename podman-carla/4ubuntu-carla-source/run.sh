0d3bfbadf07e968857652261965d216ee5a073cf
Neil Sambhu <neilsambhu@gmail.com>
2023-02-20 16:52:42 -0500

    python 3.6 to python 3.7 resolved in building carla from source in podman

--- podman-carla/4ubuntu-carla-source/run.sh
+++ podman-carla/4ubuntu-carla-source/run.sh
@@ -6,7 +6,7 @@ TAR_NAME="ubuntu-jennifer-archive.tar"
 # cleanup
 podman kill --signal KILL -a && \
 podman container cleanup --all --rm && \
-# podman rmi docker.io/library/$IMAGE_NAME
+podman rmi -f docker.io/nvidia/$IMAGE_NAME
 
 # 2/6/2023 4:18:05 PM: call Dockerfile
 podman build -t $IMAGE_NAME .

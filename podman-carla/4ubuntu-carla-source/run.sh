89c56b0d89fa08719624b40928ec78a0bd3d341a
Neil Sambhu <neilsambhu@gmail.com>
2023-03-07 12:54:09 -0500

    carla build container

--- podman-carla/4ubuntu-carla-source/run.sh
+++ podman-carla/4ubuntu-carla-source/run.sh
@@ -4,13 +4,15 @@ CONTAINER_NAME="ubuntu-jennifer2"
 TAR_NAME="ubuntu-jennifer-archive.tar"
 
 # cleanup
-# podman kill --signal KILL -a && \
-# podman container cleanup --all --rm && \
+podman kill --signal KILL -a && \
+podman container cleanup --all --rm && \
 # podman rmi -f docker.io/nvidia/$IMAGE_NAME && \
 # podman image prune --filter="dangling=true" -f
 
 # 2/6/2023 4:18:05 PM: call Dockerfile
-podman build -t $IMAGE_NAME .
+xhost local:root
+# podman build -t $IMAGE_NAME .
+podman build --compress -t $IMAGE_NAME .
 # 2/7/2023 11:50:02 AM: run image
 # podman run -d -it --name $CONTAINER_NAME nvidia/$IMAGE_NAME
 # podman run --runtime=nvidia --gpus all -d -it --name $CONTAINER_NAME nvidia/$IMAGE_NAME
@@ -18,7 +20,6 @@ podman build -t $IMAGE_NAME .
 # podman attach $CONTAINER_NAME
 
 # xhost + ; podman run -it --rm --runtime=nvidia --gpus='"device=1","capabilities=graphics,utility,display,video,compute"' --name $CONTAINER_NAME -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:2.0 nvidia/$IMAGE_NAME
-xhost local:root
 podman run --name $CONTAINER_NAME -d --privileged --net=host -e DISPLAY=$DISPLAY docker.io/nvidia/$IMAGE_NAME
 # podman attach $CONTAINER_NAME
 

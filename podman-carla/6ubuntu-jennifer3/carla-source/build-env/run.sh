a58136a4d2c4257512c81e6219409ab5dc125c0f
Neil Sambhu <neilsambhu@gmail.com>
2023-03-09 16:24:25 -0500

    container build with Jennifer

--- podman-carla/6ubuntu-jennifer3/carla-source/build-env/run.sh
+++ podman-carla/6ubuntu-jennifer3/carla-source/build-env/run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 podman build -t usf_nsambhu_carla_source /home/nsambhu/github/carla-roach/podman-carla/6ubuntu-jennifer3/carla-source/build-env
-podman run -it --rm -v /data/data1/podman_storage_hdd:/usr/src/app --entrypoint='/bin/bash' --name=usf_nsambhu_carla_build usf_nsambhu_carla_source /usr/src/install/script.sh
+podman stop usf_nsambhu_carla_build
+podman rm usf_nsambhu_carla_build
+xhost +
+podman run -it --rm --privileged --net=host -e DISPLAY=$DISPLAY -v /data/data1/podman_storage_hdd:/usr/src/app --entrypoint='/bin/bash' --name=usf_nsambhu_carla_build usf_nsambhu_carla_source

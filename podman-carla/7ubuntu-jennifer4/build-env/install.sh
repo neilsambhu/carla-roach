4537df250eb5b2094fb756a4f06d173a2e9cbd62
Neil Sambhu <neilsambhu@gmail.com>
2023-03-10 16:37:54 -0500

    ShaderCompileWorker error

--- podman-carla/7ubuntu-jennifer4/build-env/install.sh
+++ podman-carla/7ubuntu-jennifer4/build-env/install.sh
@@ -1,18 +1,18 @@
 # /bin/bash
 
-rm -rfv /usr/src/app/UnrealEngine
-cd /usr/src/app/
-unzip /usr/src/install/UnrealEngine.zip
+# rm -rfv /usr/src/app/UnrealEngine
+# cd /usr/src/app/
+# unzip /usr/src/install/UnrealEngine.zip
 
-cd UnrealEngine
-echo [exe] Setup.sh
-./Setup.sh
-echo [exe] GenerateProjectFiles.sh
-./GenerateProjectFiles.sh
-echo [exe] make -j 48
-make -j 48
-echo [exe] chmod -Rv 777 /usr/src/app/UnrealEngine
-chmod -Rv 777 /usr/src/app/UnrealEngine
+# cd UnrealEngine
+# echo [exe] Setup.sh
+# ./Setup.sh
+# echo [exe] GenerateProjectFiles.sh
+# ./GenerateProjectFiles.sh
+# echo [exe] make -j 48
+# make -j 48
+# echo [exe] chmod -Rv 777 /usr/src/app/UnrealEngine
+# chmod -Rv 777 /usr/src/app/UnrealEngine
 
 rm -rfv /usr/src/app/carla
 cd /usr/src/app/
@@ -21,7 +21,7 @@ unzip /usr/src/install/carla.zip
 export UE4_ROOT=/usr/src/app/UnrealEngine
 
 cd carla
-git checkout 0.9.13
+# git checkout 0.9.13
 echo [exe] Update.sh
 ./Update.sh
 echo [exe] make -j 48 PythonAPI
